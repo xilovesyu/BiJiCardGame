ff68340862806f26605d96bdf86508ae7899129e
xilovesyu <2211508055@ujs.edu.cn>
2016-02-23 14:51:16 +0800

    almost finish the program but need paixu and select

--- Test.java
+++ Test.java
@@ -25,7 +25,18 @@ public class Test {
 
         ///列出第一个人的牌的所有可能的组合
         ArrayList<OneCards[]> getAllCount=cardHandler.ListAllCount(p[0]);
-
-
+        /*for (OneCards[] temp:getAllCount) {
+            System.out.println(temp[0].getJifen()+","+temp[1].getJifen()+","+temp[2].getJifen());
+        }*/
+        int maxFirstOneCards=0;
+        int indexOfmaxFirst=0;
+        for (int i = 0; i < getAllCount.size(); i++) {
+            if(getAllCount.get(i)[0].getJifen()>maxFirstOneCards){
+                maxFirstOneCards=getAllCount.get(i)[0].getJifen();
+                indexOfmaxFirst=i;
+            }
+        }
+        Card[] firstcards=getAllCount.get(indexOfmaxFirst)[0].getOnecards();
+        System.out.println("第一组最大积分为"+maxFirstOneCards+","+firstcards[0].printCard()+firstcards[1].printCard()+firstcards[2].printCard());
     }
 }
