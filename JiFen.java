ec57315dcc5760cc9541277faa62334e0fde6cb2
xilovesyu <2211508055@ujs.edu.cn>
2016-02-23 13:33:11 +0800

    add JiFen annotation

---
+++ JiFen.java
@@ -0,0 +1,18 @@
+package com.xi;
+
+/**may be JiFen like a class
+ * Created by xijiaxiang on 2016/2/22.
+ */
+public class JiFen {
+    private int fenshu=0,Maxkind0=2937,Maxkind1=3146;
+    private int Maxkind2=3160,Maxkind3=6097,Maxkind4=6111;
+    public int getFenshu() {
+        return fenshu;
+    }
+    public void setFenshu(int fenshu) {
+        this.fenshu = fenshu;
+    }
+
+
+
+}
